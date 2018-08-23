fb6d72aa3d7faad810beb5dd689007c09ac1949e
Patricia Nykaza <pnykaza@calstatela.edu>
2018-08-23 20:56:43 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,7 +25,7 @@ GROUP BY (users.name);
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title,
+"SELECT projects.title
 FROM projects
 INNER JOIN pledges
 ON projects.id = pledges.project_id
