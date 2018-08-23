085daf33213f6e58ed1d9ee49ef036ae59ef90fc
Patricia Nykaza <pnykaza@calstatela.edu>
2018-08-23 19:58:28 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -2,11 +2,12 @@ CREATE TABLE project (
   id INTEGER PRIMARY KEY,
   title TEXT,
   category TEXT,
-  funding_goal INTEGER
-  starting_date
+  funding_goal INTEGER,
+  start_date TEXT,
+  end_date TEXT,
 );
 
-CREATE TABLE bears (
+CREATE TABLE user (
   id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER,
