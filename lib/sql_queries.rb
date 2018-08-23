0277986040412a02fbecd52555e3a6eb06fb9921
Patricia Nykaza <pnykaza@calstatela.edu>
2018-08-23 21:25:45 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -37,7 +37,7 @@ end
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
 "SELECT users.name, SUM(pledges.amount)
 FROM users
-LEFT OUTER JOIN pledges
+INNER JOIN pledges
 ON users.id = pledges.user_id
 GROUP BY users.name HAVING SUM(pledges.amount);
 "
