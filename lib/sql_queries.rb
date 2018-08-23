e2b1771b0b762766b2661361b55258752cadb947
Patricia Nykaza <pnykaza@calstatela.edu>
2018-08-23 22:03:40 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -39,7 +39,6 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 FROM users
 INNER JOIN pledges
 ON users.id = pledges.user_id
-GROUP BY users.name;
 ORDER BY SUM(pledges.amount);
 "
 end
