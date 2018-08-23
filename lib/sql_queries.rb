68b1f48f3d82e95e910a3ae5772ea6548f0ec51a
Patricia Nykaza <pnykaza@calstatela.edu>
2018-08-23 21:57:51 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -39,7 +39,8 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 FROM users
 LEFT OUTER JOIN pledges
 ON users.id = pledges.user_id
-GROUP BY pledges.amount;
+GROUP BY users.name;
+
 "
 end
 
